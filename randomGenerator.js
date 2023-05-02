070c07f8004c9d1e50f7ba2c12f4852ef0e7a1e2
Oscar Park <115663638+oscarpark0@users.noreply.github.com>
2023-05-02 11:13:48 -0400

    Add files via upload

---
+++ randomGenerator.js
@@ -0,0 +1,79 @@
+async function fetchTrueRandomString(length, allowedCharacters) {
+  const apiKey = process.env.API_KEY; // Get API key from environment variable
+  const url = `https://api.random.org/json-rpc/2/invoke`;
+
+  const requestData = {
+    jsonrpc: "2.0",
+    method: "generateStrings",
+    params: {
+      apiKey: apiKey,
+      n: 1,
+      length: length,
+      characters: allowedCharacters,
+      replacement: true
+    },
+    id: 1
+  };
+
+  const response = await fetch(url, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify(requestData)
+  });
+
+  const data = await response.json();
+
+  // Check for error in the API response
+  if (data.error) {
+    throw new Error(`API Error: ${data.error.message}`);
+  }
+
+  // Return the generated random string
+  return data.result.random.data[0];
+}
+
+window.generateTrueRandomBinaryString = async function (length) {
+  return await fetchTrueRandomString(length, "01");
+};
+
+window.generateTrueRandomText = async function (length) {
+  const characters =
+    'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()_+=-:;",<.>/?`~';
+  return await fetchTrueRandomString(length, characters);
+};
+
+async function generateBinaryString2() {
+  var length = 32; // change it to the desired length
+  var result = await window.generateTrueRandomBinaryString(length);
+  var resultDiv = document.getElementById("result");
+  animateSplitFlap(resultDiv, result);
+}
+
+async function generateRandomText2() {
+  var length = 1000; // change it to the desired length
+  var result = await window.generateTrueRandomText(length);
+  var resultDiv = document.getElementById("result");
+  animateSplitFlap(resultDiv, result);
+}
+
+
+function animateSplitFlap(container, text) {
+  const oldSpan = container.querySelector(".split-flap span");
+  if (oldSpan) {
+    splitflap.stop();
+    container.removeChild(oldSpan);
+  }
+
+  const newSpan = document.createElement("span");
+  newSpan.style.top = "100%";
+  container.querySelector(".split-flap").appendChild(newSpan);
+
+  splitflap(newSpan, [text], {
+    'timeOut' : 3000,
+    'tickTimeOut' : 60,
+    'nbJumpIterations' : 10,
+  });
+}
+
---
+++ splitflap.js
@@ -0,0 +1,160 @@
+/**
+ * Adds a replaceAt function to strings that takes in an index and a replacement character
+ *
+ */
+String.prototype.replaceAt=function(index, replacement) {
+  let left;
+  let right;
+
+  if(this.substr(0,index) == undefined){
+    left = "";
+  }
+  else
+  {
+    left = this.substr(0,index);
+  }
+
+  if(this.substr(index + replacement.length) == undefined ){
+    right="";
+  }
+  else{
+    right = this.substr(index + replacement.length);
+  }
+  return left + replacement + right;
+}
+
+let splitFlapDefaults = {
+  'timeOut' : 2000,
+  'tickTimeOut' : 60,
+  'nbJumpIterations' : 4,
+};
+
+/**
+ *
+*/
+function splitFlap(domElement, texts, options = splitFlapDefaults ){
+
+  let curIndex = -1;
+  let curDoneIterations = 0;
+  let curText;
+  let curEndText;
+
+  const nbJumpIterations = options.nbJumpIterations;
+  const timeOut = options.timeOut;
+  const tickTimeOut = options.tickTimeOut;
+
+  setTimeout(changeText, timeOut);
+
+  function changeText(){
+
+    let beforeText;
+    let afterText;
+
+    if(curIndex >= 0){
+      beforeText = texts[curIndex];
+      if(curIndex == texts.length-1){
+        afterText = texts[0];
+      }
+      else{
+        afterText = texts[curIndex+1];
+      }
+    }
+    else
+    {
+      beforeText = domElement.innerText;
+      afterText = texts[0];
+    }
+    updateIndex();
+    transitionText(beforeText, afterText);
+  }
+
+  function updateIndex(){
+    if(curIndex < texts.length-1)
+    {
+      curIndex++;
+    }
+    else
+    {
+      curIndex = 0;
+    }
+  }
+
+  function transitionText(startText, endText){
+    curText = startText;
+
+    if(endText.length < curText.length){
+      let diff = curText.length-endText.length;
+      curEndText = endText+" ".repeat(diff);
+    }
+    else
+    {
+      curEndText = endText;
+    }
+    transitionTextTick();
+  }
+
+  function transitionTextTick(){
+    let endLength;
+    let startLength;
+
+    if(curText.length == null){
+      startLength=0;
+    }
+    else
+    {
+      startLength = curText.length;
+    }
+    if(curEndText.length == null){
+      endLength=0;
+    }
+    else
+    {
+      endLength = curEndText.length;
+    }
+
+    var longestLength = (startLength >= endLength ? startLength : endLength);
+
+    for (var i = 0; i < longestLength; i++) {
+      let curCharCode = curText.charCodeAt(i);
+
+      if(curCharCode == undefined || isNaN(curCharCode))
+      {
+        curCharCode = " ".charCodeAt(0);
+        curText = curText.replaceAt(i," ");
+      }
+      else
+      {
+        if(curText.charAt(i)!=curEndText.charAt(i)){
+          if(curCharCode >= "z".charCodeAt(0) || curCharCode < " ".charCodeAt(0))
+          {
+            curCharCode = " ".charCodeAt(0);
+          }
+          else{
+            curCharCode ++;
+          }
+          curText = curText.replaceAt(i,String.fromCharCode(curCharCode));
+        }
+      }
+    }
+
+    if(curDoneIterations >= nbJumpIterations || curText.trim().localeCompare(curEndText.trim()) == 0 ){
+      domElement.innerText=curText;
+
+      if(curText.trim().localeCompare(curEndText.trim()) != 0)
+{
+setTimeout(transitionTextTick, tickTimeOut);
+}
+else
+{
+setTimeout(changeText,timeOut);
+}
+curDoneIterations = 0;
+}
+else
+{
+curDoneIterations++;
+setTimeout(transitionTextTick, tickTimeOut);
+}
+}
+}
+module.exports = { splitFlap };
